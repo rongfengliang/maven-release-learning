656ab26d19be8aceead6ebdc8927ad13da884227
rongfengliang <1141591465@qq.com>
2019-11-25 17:39:47 +0800

    do some test

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,9 @@
 
 public class Application {
     public static void main(String[] args) {
-
+        /*
+         fetch java config
+         */
         String path = System.getProperty("PATH");
         System.out.println(path);
 
